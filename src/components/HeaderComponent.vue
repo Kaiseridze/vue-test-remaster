28f4737e0881d5385f4a598fc13497d298b61389
Kaiseridze <kaiseidze@gmail.com>
2023-02-08 18:49:33 +0300

    added icon assets

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="header">
     <nav class="header__nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/settings">Settings</router-link>
+      <router-link class="header__item" to="/"><img src="@/assets/home.svg" alt="home"></router-link>
+      <router-link class="header__item" to="/settings"><img src="@/assets/gear.svg" alt="gear"></router-link>
     </nav>
   </header>
 </template>
@@ -17,11 +17,14 @@ export default defineComponent({
 
 <style scoped lang="scss">
 .header {
-  font-family: 'Poppins', sans-serif;
+  &__item {
+    color: #000;
+  }
 
   &__nav {
     display: flex;
     justify-content: space-between;
+    align-items: center;
   }
 }
 </style>
